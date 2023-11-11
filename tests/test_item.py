56d4b4d7379453bc26b8cd3fdcc7bc456bbfc5c6
Mikhail Glushkov <miggl@mail.ru>
2023-11-11 16:00:36 +0300

    HomeWork 13_1 Part_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+
+item = Item('Ноутбук', 100000, 5)
+assert item.name == 'Ноутбук'
+assert item.price == 100000
+assert item.quantity == 5
+
+assert item.calculate_total_price() == 500000
+
+Item.pay_rate = 0.7
+item.apply_discount()
+assert item.price == 70000
