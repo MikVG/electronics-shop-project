5a9b8efd97fcad200ae01d55ca630bceb9ddd41d
Mikhail Glushkov <miggl@mail.ru>
2023-11-12 15:39:53 +0300

    Fix test for HW 13_1 Task_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,15 +1,24 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-
+import pytest
 from src.item import Item
 
 
-item = Item('Ноутбук', 100000, 5)
-assert item.name == 'Ноутбук'
-assert item.price == 100000
-assert item.quantity == 5
+@pytest.fixture
+def item_class():
+    return Item('Ноутбук', 100000, 5)
 
-assert item.calculate_total_price() == 500000
 
-Item.pay_rate = 0.7
-item.apply_discount()
-assert item.price == 70000
+def test_class(item_class):
+    assert item_class.name == 'Ноутбук'
+    assert item_class.price == 100000
+    assert item_class.quantity == 5
+
+
+def test_calculate(item_class):
+    assert item_class.calculate_total_price() == 500000
+
+
+def test_discount(item_class):
+    Item.pay_rate = 0.7
+    item_class.apply_discount()
+    assert item_class.price == 70000
