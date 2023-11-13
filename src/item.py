d97aff142d83fb825ac54e0882451f00240389c3
Mikhail Glushkov <miggl@mail.ru>
2023-11-14 01:30:00 +0300

    HomeWork 13_2 Task_1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -14,10 +18,30 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+        print(self.__name)
+
+    @property
+    def name(self):
+        """
+        геттер для приватного атрибута __name класса Item
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        cеттер для приватного атрибута __name класса Item.
+        Проверяет, что длина наименования товара (атрибут __name) не превышает 10 симвовов.
+        В случае превышения, обрезает строку до первых 10 символов.
+        """
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
 
 
     def calculate_total_price(self) -> float:
@@ -34,3 +58,31 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, file):
+        """
+        класс метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
+        """
+        cls.all.clear()
+        absolutely_dir = os.path.dirname(__file__).replace('src', '')
+        url = absolutely_dir + file
+        with open(url, encoding='windows-1251') as f:
+            csv_dict = csv.DictReader(f, delimiter=',')
+            for text in csv_dict:
+                name = text['name']
+                price = cls.string_to_number(text['price'])
+                quantity = cls.string_to_number(text['quantity'])
+                item_csv = Item(name, price, quantity)
+            return item_csv
+
+    @staticmethod
+    def string_to_number(string):
+        """
+        статический метод, возвращающий число из числа-строки
+        """
+        if string.isdigit():
+            string = int(string)
+        else:
+            string = int(float(string))
+        return string
--- tests/test_item.py
+++ tests/test_item.py
@@ -22,3 +22,17 @@ def test_discount(item_class):
     Item.pay_rate = 0.7
     item_class.apply_discount()
     assert item_class.price == 70000
+
+
+def test_class_2():
+    item_class_2 = Item('Телефон', 10000, 5)
+    assert item_class_2.name == 'Телефон'
+    assert item_class_2.price == 10000
+    assert item_class_2.quantity == 5
+
+
+def test_name_len():
+    item_len = Item('Ноутбук', 100000, 5)
+    item_len.name = 'СуперНоутбук'
+    assert item_len.name == 'СуперНоутб'
+
